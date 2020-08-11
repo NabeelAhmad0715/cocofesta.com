9fe7b76741d8147addec33ffa56609e07da1d503
Nabeel Amjad <nabeel@bloomrix.com>
2020-08-12 00:20:08 +0500

    chat system

--- resources/views/frontend/layouts/layout.blade.php
+++ resources/views/frontend/layouts/layout.blade.php
@@ -335,7 +335,20 @@
     <!-- custom -->
     <script src="{{ asset('js/custom.js') }}"></script>
 
-    <script type="text/javascript">
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5f3109b2ed9d9d26270988ba/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+    {{-- <script type="text/javascript">
         (function () {
             var options = {
                 facebook: "103233641397198", // Facebook page ID
@@ -353,7 +366,7 @@
             s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
             var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
         })();
-      </script>
+      </script> --}}
       @stack('scripts')
 </body>
 </html>
